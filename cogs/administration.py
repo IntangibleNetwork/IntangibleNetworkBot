447fe7e19bea4dc870fe0a48f545c266507aeb22
GiraffeKey <giraffekey@tutanota.com>
2020-04-03 19:27:55 -0700

    Add commands for kick, ban, and unban

--- cogs/administration.py
+++ cogs/administration.py
@@ -28,10 +28,64 @@ class Administration(commands.Cog):
                                   color=0xff0007)
             await ctx.channel.send(embed=embed)
 
-    @commands.command(name="ping")
+    @commands.has_permissions(kick_members=True)
+    @commands.command(name='kick')
+    async def kick(self, ctx, member : discord.Member, *, reason=None):
+        await member.kick(reason=reason)
+        await ctx.send(self.gen_msg('kicked', member, reason))
+
+    @commands.has_permissions(ban_members=True)
+    @commands.command(name='ban')
+    async def ban(self, ctx, member : discord.Member, *, reason=None):
+        await member.ban(reason=reason)
+        await ctx.send(self.gen_msg('banned', member, reason))
+
+    @commands.has_permissions(ban_members=True)
+    @commands.command(name='unban')
+    async def unban(self, ctx, *, member):
+        banned_list = await ctx.guild.bans()
+        member_name, member_id = member, None
+        if '#' in member:
+            member_name, member_id = member.split('#')
+
+        found = False
+
+        for ban_entry in banned_list:
+            user = ban_entry.user
+            if user.name == member_name and member_id == user.discriminator or member_id == None:
+                await ctx.guild.unban(ban_entry.user)
+                await ctx.send(f'{user.mention} has been unbanned.')
+                found = True
+                break
+        
+        if not found:
+            await ctx.send(f'Could not find {member}.')
+
+    @commands.has_permissions(ban_members=True)
+    @commands.command(name='tempban')
+    async def tempban(self, ctx, member : discord.Member, *, reason=None):
+        await ctx.send(self.gen_msg('banned', member, reason))
+
+    @is_bot_owner_check()
+    @commands.command(name='mute')
+    async def mute(self, ctx, member : discord.Member, *, reason=None):
+        await ctx.send(self.gen_msg('muted', member, reason))
+
+    @is_bot_owner_check()
+    @commands.command(name='tempmute')
+    async def tempmute(self, ctx, member : discord.Member, *, reason=None):
+        await ctx.send(self.gen_msg('muted', member, reason))
+
+    @commands.command(name='ping')
     async def ping(self, ctx):
-        await ctx.send("Pong!")
-        await ctx.add_reaction(":ping_pong:")
+        await ctx.send('Pong! {}')
+
+    def gen_msg(self, verb, member, reason):
+        msg = f'{member.mention} was {verb}'
+        if reason != None:
+            msg += f' for {reason}'
+        msg += '!'
+        return msg
 
 
 def setup(bot):
